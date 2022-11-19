ab14ec000de10129845f816cd5f03d37c36cfc85
Stephane Gelibert <stephane.gelibert@epita.fr>
2022-11-19 19:32:19 +0100

    feat: bfv implementation

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 import numpy as np
 from Pyfhel import Pyfhel
-HE = Pyfhel()           # Creating empty Pyfhel object
-HE.contextGen(scheme='bfv', n=2**14, t_bits=40)  # Generate context for 'bfv'/'ckks' scheme
-                        # The n defines the number of plaintext slots.
-                        #  There are many configurable parameters on this step
-                        #  More info in Demo_2, Demo_3, and Pyfhel.contextGen()
-HE.keyGen()             # Key Generation: generates a pair of public/secret keys
-integer1 = np.array([64], dtype=np.int64)
-integer2 = np.array([-2], dtype=np.int64)
-ctxt1 = HE.encryptInt(integer1) # Encryption makes use of the public key
-ctxt2 = HE.encryptInt(integer2) # For integers, encryptInt function is used.
-ctxtSum = ctxt1 + ctxt2         # `ctxt1 += ctxt2` for inplace operation
-ctxtSub = ctxt1 - ctxt2         # `ctxt1 -= ctxt2` for inplace operation
-ctxtMul = ctxt1 * ctxt2         # `ctxt1 *= ctxt2` for inplace operation
-ctxtDiv = ctxt1 / 2
-resSum = HE.decryptInt(ctxtSum) # Decryption must use the corresponding function
-                                #  decryptInt.
-resSub = HE.decryptInt(ctxtSub)
-resMul = HE.decryptInt(ctxtMul)
-resDiv = HE.decryptInt(ctxtDiv)
-print("#. Decrypting result:")
-print("     addition:       decrypt(ctxt1 + ctxt2) =  ", resSum)
-print("     substraction:   decrypt(ctxt1 - ctxt2) =  ", resSub)
-print("     multiplication: decrypt(ctxt1 + ctxt2) =  ", resMul)
-print("     division:       decrypt(ctxt1 / ctxt2) =  ", resDiv)
+HE = Pyfhel()
+bfv_params = {
+    'scheme': 'BFV',
+    'n': 2**13,
+    't': 65537,
+    't_bits': 20,
+    'sec': 128,
+}
+HE.contextGen(**bfv_params)
+HE.keyGen()
+HE.rotateKeyGen()
+HE.relinKeyGen()
+
+a = np.array([60], dtype=np.int64)
+sys = np.array([120], dtype=np.int64)
+dia = np.array([70], dtype=np.int64)
+chol = np.array([200], dtype=np.int64)
+ht = np.array([175], dtype=np.int64)
+wt = np.array([70], dtype=np.int64)
+
+ctx_a = HE.encryptInt(a)
+ctx_sys = HE.encryptInt(sys)
+ctx_dia = HE.encryptInt(dia)
+ctx_chol = HE.encryptInt(chol)
+ctx_ht = HE.encryptInt(ht)
+ctx_wt = HE.encryptInt(wt)
+
+z = 72 * ctx_a + 13 * ctx_sys - 29 * ctx_dia + 8 * ctx_chol - 53 * ctx_ht + 21 * ctx_wt
+
+fz = -17 * z**7 + 168 * z**5 - 1680 * z**3 + 20160 * z + 403200
+
+print(HE.decryptInt(z))
